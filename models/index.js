a270aefaa093e67595d3966c49311c9ba09aa892
jacksonjjones <jacksonjjones1@gmail.com>
2024-03-26 10:26:25 -0500

    completed models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,23 @@
 const User = require('./User');
+const Blog = require('./blog');
+const Comment = require('./comment');
 
-module.exports = { User };
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment,{
+  foreignKey: 'blog_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
+
